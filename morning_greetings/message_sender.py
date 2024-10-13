021dde04f5c866bc5370aa158c7bee9e3fdd3f32
Martin Solevåg Glærum <74411819+martinglaerum@users.noreply.github.com>
2024-10-13 19:03:26 +0200

    Update message_sender.py

--- morning_greetings/message_sender.py
+++ morning_greetings/message_sender.py
@@ -1,5 +1,6 @@
 def send_message(email, message):
-
+    '''Sends a message to the specified email'''
+        
         # Checks that there is an email specified
     if not email:
         raise ValueError("Email address is missing")

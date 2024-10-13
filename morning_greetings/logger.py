3dd90ef0cdb2de688b2725429e5fc79b4047bd1b
Martin Solevåg Glærum <74411819+martinglaerum@users.noreply.github.com>
2024-10-13 17:32:39 +0200

    Update logger.py

--- morning_greetings/logger.py
+++ morning_greetings/logger.py
@@ -7,6 +7,6 @@ BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 def log_message(contact, message):
     file_path = os.path.join(BASE_DIR, "message_log.txt") # Specifies the path to the log file
 
-        # Opens the log file and stores the message sent, inlcuding who it was sent to and when it was sent
+        # Opens the log file and stores the message sent, including who it was sent to and when it was sent
     with open(file_path, "at") as f:
         f.write(f"Time: {datetime.datetime.now()}, The following message was sent to {contact}: {message}\n")

87f63413aaf08390c8e553d81f2779c5494b1e47
Martin Solevåg Glærum <74411819+martinglaerum@users.noreply.github.com>
2024-10-13 11:31:32 +0200

    Update contact_manager.py

--- morning_greetings/contact_manager.py
+++ morning_greetings/contact_manager.py
@@ -3,12 +3,14 @@ import os
     # Absolute path to the current file's directory
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 
-# contacts_manager.py
 class ContactsManager:
+        # Dictionary containing the contacts
     contacts = []
+
+        # Initializes the contacts dictionary
     def __init__(self):
         contacts = []
-            # Finds the current file path of the file containing the contacts
+            # Finds the current file path for the file containing the contacts
         file_path = os.path.join(BASE_DIR, "contacts.txt")
 
             # Opens and reads all contacts stored in the contacts.txt file
@@ -21,30 +23,40 @@ class ContactsManager:
                     'preferred_time': preferred_time
                 })
 
-    def add_contact(self, name, email, preferred_time="08:00 AM"):
+        # Adds a new contact to the contacts dictionary
+    def add_contact(self, name, email, preferred_time="08:00 AM"): 
         self.contacts.append({
                     'name': name,
                     'email': email,
                     'preferred_time': preferred_time
                 })
+        
+            # Saves the new contact to the contacts.txt file
         self.save_contacts()
 
-    def remove_contact(self, name):
+        # Removes a contact if the inputted name matches an existing contact
+    def remove_contact(self, name): 
         self.contacts = [c for c in self.contacts if c['name'] != name]
+
+            # Saves the new contact to the contacts.txt file
         self.save_contacts()
 
-    def get_contacts(self):
+        # Returns the contacts dictionary
+    def get_contacts(self):  
         return self.contacts
 
-        # Writes changes to the contacts to the contact.txt file
+        # Saves changes in the contacts dictionary to the contact.txt file
     def save_contacts(self):
+            # Finds the file path to the contacts.txt file
         file_path = os.path.join(BASE_DIR, "contacts.txt")
 
+            # Writes the contacts dictionary to the contacts.txt file
         with open(file_path, 'wt') as file:
             for contact in self.contacts:
-                # Formatting each contact dictionary as a line
+                    # Formats each contact as a line with specific syntax so the file can easily be read
                 file.write(f"{contact['name']},{contact['email']},{contact['preferred_time']}\n")
 
+        # Lists all the contacts in the contacts dictionary
     def list_contacts(self):
         for contact in self.contacts:
             print(f"Name: {contact['name']}, Email: {contact['email']}, Preferred Time: {contact['preferred_time']}")
@@ -60,15 +72,16 @@ def manage():
     print("3. List all contacts")
     choice = input("Enter the representing your choice: ")
 
-    if choice == '1':
+        
+    if choice == '1': # Creates a new contact
         name = input("Name of the new contact: ")
         email = input(f"{name}'s email: ")
         preferred_time = input(f"{name}'s preffered time to be contacted: ")
         Contacts.add_contact(name, email, preferred_time)
-    elif choice == '2':
+    elif choice == '2':  # Removes a contact
         name = input("Name of the contact to remove: ")
         Contacts.remove_contact(name)
-    elif choice == '3':
+    elif choice == '3': # Lists all contacts
         Contacts.list_contacts()
     else:
         print("Invalid choice. Please choose a valid number.")
